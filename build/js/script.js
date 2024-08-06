8273d6b387483b24d0ec30c6a9a6c3ea96e24f75
Kseniia Meshkova <dr.meshkovak@gmail.com>
2024-08-06 21:04:24 +0400

    Оформляет каталог

--- build/js/script.js
+++ build/js/script.js
@@ -1,9 +1,26 @@
 const nav = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 
+const sliderHandle = document.querySelector('.slider__handle');
+const slider = document.querySelector('.slider');
+
 nav.classList.add('main-nav--closed');
 
-navToggle.addEventListener('click' , () => {
+navToggle.addEventListener('click', () => {
     nav.classList.toggle('main-nav--closed');
     nav.classList.toggle('main-nav--opened');
 });
+
+if (sliderHandle) {
+    let flag = false;
+    sliderHandle.addEventListener('click', () => {
+        if (!flag) {
+            slider.classList.add('.slider--after');
+            flag = true;
+        } else {
+            slider.classList.toggle('.slider--after');
+            slider.classList.toggle('.slider--before');
+        }
+
+    })
+}
